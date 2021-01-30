665c741b025d7a7e87b649eeac50990f58628ef4
boushib <boushib01@gmail.com>
2021-01-30 14:10:38 +0100

    remove the old dummy job

--- src/store/reducers/jobsReducer.ts
+++ src/store/reducers/jobsReducer.ts
@@ -3,20 +3,6 @@ const initialState = {
   jobs: [],
 }
 
-const dummyJob = {
-  id: '1',
-  created_at: 'string',
-  type: 'string',
-  title: 'just testing',
-  description: 'string',
-  company: 'string',
-  company_logo: 'string',
-  company_url: 'string',
-  location: 'string',
-  url: 'string',
-  how_to_apply: 'string',
-}
-
 const jobsReducer = (state: JobsState = initialState, action: { type: Action, payload: any }) => {
   switch (action.type) {
     case Action.FETCH_JOBS:
