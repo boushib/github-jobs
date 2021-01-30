47cb484b0f6e23a1694d4678f94382cc451b9bbc
boushib <boushib01@gmail.com>
2021-01-30 14:06:16 +0100

    remove the proxy API based base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-  //baseURL: 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
   baseURL: 'https://jobs.github.com/positions.json',
   cancelToken: axios.CancelToken.source().token,
 })
